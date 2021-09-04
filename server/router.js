6f5f6f03cbda5808836ca26e999101b3f682217f
Andrew Munoz <ajmunoz411@gmail.com>
2021-09-03 20:15:13 -0700

    getTracks model/controller implemented

--- server/router.js
+++ server/router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const dbhelpers = require('../db/dbhelpers');
 const spothelpers = require('./spothelpers');
+const controllers = require('../db/controllers/controllers');
 
 router
-  .get('/db/:country/:quantity', dbhelpers.getTracks)
+  // .get('/db/:country/:quantity', dbhelpers.getTracks)
+  .get('/db/:countryCode/:quantity', controllers.getTracks)
   .get('/track', spothelpers.getTrack)
   .get('/list/tracks/:list', spothelpers.getTracksList)
   .get('/features/:list', spothelpers.getTracksFeatures)
